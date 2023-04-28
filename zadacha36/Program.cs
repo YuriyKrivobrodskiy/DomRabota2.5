a83b9d73311fb7802e1e578d056e5fc9dc3ff7de
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-04-28 18:03:40 +0300

    zadacha36

---
+++ zadacha36/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+
+int sum = 0;
+
+ for (int i = 1; i < numbers.Length; i += 2)
+   {
+      sum += numbers[i];
+   }
+
+Console.WriteLine($"Сумма элементов с нечётным индексом равна: {sum} .");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 99);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
